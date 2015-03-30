dcc8eb0c8de8d21954f99d8b49653d2bd212369f
Martin Owens <doctormo@gmail.com>
2015-03-30 14:23:17 -0400

    Fix static

--- hoodcms/urls.py
+++ hoodcms/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/',   include(admin.site.urls)),
     url(r'^',         include('cms.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
