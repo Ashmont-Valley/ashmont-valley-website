9c1d43fbb1680042a6907cdd3db0dce148ffdfbd
Martin Owens <doctormo@gmail.com>
2015-03-30 12:50:09 -0400

    Fix for hoodcms

--- hoodcms/wsgi.py
+++ hoodcms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for boostcms project.
+WSGI config for hoodcms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boostcms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoodcms.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
