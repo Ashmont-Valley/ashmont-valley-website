3d1e1d0c11ec6e07f0ada768ab7ea8f70d01b924
vpeguero <vpeguero16@gmail.com>
2015-04-05 18:10:36 -0400

    Added django-calendarium

---
+++ django-calendarium/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+import calendarium
+try:
+    import multiprocessing  # NOQA
+except ImportError:
+    pass
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name="django-calendarium",
+    version=calendarium.__version__,
+    description=read('DESCRIPTION'),
+    long_description=read('README.rst'),
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='django, calendar, app, widget, events, schedule',
+    author='Daniel Kaufhold',
+    author_email='daniel.kaufhold@bitmazk.com',
+    url="https://github.com/bitmazk/django-calendarium",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.5',
+        'South',
+        'python-dateutil',
+        'django-filer',
+    ],
+    tests_require=[
+        'fabric',
+        'factory_boy',
+        'django_libs',
+        'django-nose',
+        'coverage',
+        'django-coverage',
+        'mock',
+    ],
+    test_suite='calendarium.tests.runtests.runtests',
+)
