606a33ed6eebe8f3f70ffc771a744cda5f3e12ef
Martin Owens <doctormo@gmail.com>
2015-05-29 16:12:25 -0400

    Clean forms

--- meetings/forms.py
+++ meetings/forms.py
@@ -2,8 +2,6 @@ from django.forms import ModelForm
 
 from django.utils.translation import ugettext_lazy as _
 
-from meetings.models import *
-
 class MeetingCreateForm(ModelForm):
     class Meta:
         model = Meeting
@@ -13,11 +11,9 @@ class MeetingCreateForm(ModelForm):
                 }
 
 class MeetingEditForm(ModelForm):
-    chair = models.ForeignKey(Person, related_name="chair",
-        help_text=_("chair of the meeting"), blank=True, null=True)
-
     class Meta:
         model = Meeting
         fields = ['start_time', 'end_time', 'chair', 'secretary',
             'people_attending', 'people_absent', 'people_guests']#,'notes']
+
 
