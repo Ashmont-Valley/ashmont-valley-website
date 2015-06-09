9de7ce3095b284a4a5730aa88f469de270ecd417
jdynes <jdynes@andrew.cmu.edu>
2015-06-09 13:43:26 -0400

    Fixed bug in deletion of dynamically created notes.

--- meetings/static/js/meetings.js
+++ meetings/static/js/meetings.js
@@ -1,47 +1,40 @@
+function add_note(event) {
+  $.ajax({
+    method: this.method,
+    url:    this.action,
+    data:   $(this).serialize(),
+    success: function(response) {
+      $(response).insertBefore('#add_note'); 
+      $('#note_text_box').val("");
+    },
+    error: function(response) {
+      document.write(response);
+    }
+  });
+  return false;
+};
+
+function delete_note(event) {
+  $.ajax({
+    method: this.method,
+    url:    this.action,
+    data:   $(this).serialize(),
+    success: function(response) {
+      $("#mnote-".concat(response)).remove();
+    },
+    error: function(response) {
+      document.write(response);
+    }
+  });
+  return false;
+};
 
 $(document).ready(function() {
 
-    // Provide ajax submission support
-    $('#add_note').click(function(event) {$(this).data('clicked',$(event.target))});
-    $('#add_note').submit(function(event) {
-      var button = $(this).data('clicked');
-      if(button.data('ajax')) {
-        $.ajax({
-          method: this.method,
-          url:    this.action,
-          data:   $(this).serialize(),
-          success: function(response) {
-            $(response).insertBefore('#add_note');
-            $('#note_text_box').val("");
-          },
-          error: function(response) {
-            document.write(response);
-          }
-        });
-        return false;
-      }
-      return true;
-    });
+  // Provide ajax submission support
+  $('#add_note').submit(add_note)
 
-    $('.delete_note').click(function(event) {$(this).data('clicked',$(event.target))});
-    $('.delete_note').submit(function(event) {
-      var button = $(this).data('clicked');
-      if(button.data('ajax')) {
-        $.ajax({
-          method: this.method,
-          url:    this.action,
-          data:   $(this).serialize(),
-          success: function(response) {
-            $("#mnote-".concat(response)).remove();
-          },
-          error: function(response) {
-            document.write(response);
-          }
-        });
-        return false;
-      }
-      return true;
-    });
+  $('#notes').on('submit', '.delete_note', delete_note)
 
 });
 
